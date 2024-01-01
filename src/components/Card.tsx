b28d1eec5508d903b3deb23dbcad067e0a32ebb8
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-01-01 19:35:39 +0800

    Add hover styles

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,14 +1,26 @@
+import { MouseEvent, useRef } from 'react';
 import { BlogPreview } from '../definitions';
 import './Card.css';
 import Tag from './Tag';
 
 function Card({ item }: { item: BlogPreview }) {
+  const link = useRef<HTMLAnchorElement>(null);
+  const card = useRef<HTMLElement>(null);
+
   const formattedDate = new Intl.DateTimeFormat('en-GB', {
     dateStyle: 'medium',
   }).format(new Date(item.datePublished));
 
+  const onMouseOver = (e: MouseEvent) => {
+    if (e.target === link.current) {
+      card.current?.classList.add('hover');
+    } else {
+      card.current?.classList.remove('hover');
+    }
+  };
+
   return (
-    <article className="card">
+    <article className="card" onMouseOver={onMouseOver} ref={card}>
       <div className="image">
         <img src={item.image} alt="" />
       </div>
@@ -21,7 +33,11 @@ function Card({ item }: { item: BlogPreview }) {
         <p className="pubdate">
           Published <time dateTime={item.datePublished}>{formattedDate}</time>
         </p>
-        <h2 className="title">{item.title}</h2>
+        <h2 className="title">
+          <a href="#" ref={link}>
+            {item.title}
+          </a>
+        </h2>
         <p className="description">{item.description}</p>
         <p className="byline">
           <img src={item.author.image} alt="" width="32" height="32" />
